ee546ac2d757339e77d1e749e7c7ff66e81b98d7
Ryan Victory <ryan@beerandraptors.com>
2023-10-02 16:01:45 -0500

    Add mitmproxy addon

--- mitmproxy/addon.py
+++ mitmproxy/addon.py
@@ -12,12 +12,12 @@ class HTTPRewriter:
     def response(self, flow):
         ctx.log.info("Host: ")
         ctx.log.info(flow.request.pretty_host)
-        if flow.request.pretty_host == "example.com":
+        if True: #flow.request.pretty_host == "example.com":
             ctx.log.info("Rewriting response from example.com")
-            ctx.log.info(flow.response.content.decode("utf-8"))
             flow.response.content = bytes(flow.response.content.decode("utf-8").replace(
-                        "domain", "dog"
-                    ), "utf-8")
+                        "the", "THE"
+                    ),
+            "utf-8")
         self.num = self.num + 1
         ctx.log.info("We've seen %d flows" % self.num)
 
