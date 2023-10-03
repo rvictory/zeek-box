bea943ea427ce1f6dc608df592852c524fe2ab25
Ryan Victory <ryan@beerandraptors.com>
2023-10-03 08:36:47 -0500

    Increase IP Rotation Sleep duration

--- waveshare_integration/eink_info.py
+++ waveshare_integration/eink_info.py
@@ -70,7 +70,7 @@ def printToDisplay(string):
 def rotate_vpn():
     printToDisplay("Rotating VPN...")
     os.system("ruby /opt/utils/rotate_vpn.rb us")
-    time.sleep(2)
+    time.sleep(5)
     print_ip_info()
 
 def print_ip_info():
